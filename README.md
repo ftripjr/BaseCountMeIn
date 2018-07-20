6c05b8d6ec15f074dad7c68b903547570df00cf5
ftripjr <frederic.triplett@gmail.com>
2018-07-20 13:56:38 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# baseCountMeIn
---
+++ baseCountMeIn/CategoryViewController.swift
@@ -0,0 +1,28 @@
+//
+//  CategoryViewController.swift
+//  baseCountMeIn - View Controller for
+//      category selection
+//
+//  Created by Freddie Triplett on 7/20/18.
+//  Copyright © 2018 Freddie Triplett. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CategoryViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+}
+
